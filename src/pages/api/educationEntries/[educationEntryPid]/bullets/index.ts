b924c68ef57bd220e8a3fc698165eaac960ba7d7
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-17 10:25:04 -0700

    Add APIs for education entry bullets

---
+++ src/pages/api/educationEntries/[educationEntryPid]/bullets/index.ts
@@ -0,0 +1,35 @@
+import type { NextApiResponse } from "next";
+import { EducationEntryBullet, Prisma } from "@/generated/prisma";
+import { makeApiHandler, prisma, sendError, sendResponse } from "@/lib";
+
+export default makeApiHandler({
+  POST: async (req, res: NextApiResponse<EducationEntryBullet>) => {
+    const educationEntryPid = req.query.educationEntryPid as string;
+    const maxSortOrderEntry = await prisma.educationEntryBullet.findFirst({
+      where: { educationEntry: { pid: educationEntryPid } },
+      orderBy: { sortOrder: "desc" },
+    });
+    const sortOrder = (maxSortOrderEntry?.sortOrder ?? -1) + 1;
+    try {
+      const educationEntryBullet = await prisma.educationEntryBullet.create({
+        data: {
+          text: "",
+          enabled: true,
+          sortOrder,
+          educationEntry: {
+            connect: { pid: educationEntryPid },
+          },
+        },
+      });
+      return sendResponse(res, 201, educationEntryBullet);
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        return sendError(res, 404);
+      }
+      return sendError(res, 500);
+    }
+  },
+});
---
+++ src/pages/api/educationEntryBullets/[educationEntryBulletPid].ts
@@ -0,0 +1,42 @@
+import { EducationEntryBullet, Prisma } from "@/generated/prisma";
+import { makeApiHandler, prisma, sendError, sendResponse } from "@/lib";
+import { NextApiResponse } from "next";
+
+export default makeApiHandler({
+  PUT: async (req, res: NextApiResponse<EducationEntryBullet>) => {
+    const educationEntryBulletPid = req.query.educationEntryBulletPid as string;
+    try {
+      const educationEntryBullet = await prisma.educationEntryBullet.update({
+        where: { pid: educationEntryBulletPid },
+        data: req.body,
+      });
+      return res.status(200).json(educationEntryBullet);
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        return sendError(res, 404);
+      }
+      return sendError(res, 500);
+    }
+  },
+
+  DELETE: async (req, res: NextApiResponse<void>) => {
+    const educationEntryBulletPid = req.query.educationEntryBulletPid as string;
+    try {
+      await prisma.educationEntryBullet.delete({
+        where: { pid: educationEntryBulletPid },
+      });
+      return sendResponse(res, 204);
+    } catch (error) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === "P2025"
+      ) {
+        return sendError(res, 404);
+      }
+      return sendError(res, 500);
+    }
+  },
+});
