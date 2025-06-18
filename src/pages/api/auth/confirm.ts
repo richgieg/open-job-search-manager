2d4d44bf0854e8f4752fe1f6c0a006c60bc6a01e
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-18 09:25:16 -0700

    Use makeApiHandler for /auth/confirm

--- src/pages/api/auth/confirm.ts
+++ src/pages/api/auth/confirm.ts
@@ -1,39 +1,33 @@
 import { type EmailOtpType } from "@supabase/supabase-js";
-import type { NextApiRequest, NextApiResponse } from "next";
 
 import { createClient } from "@/lib/supabase/api";
+import { makeApiHandler } from "@/lib";
 
 function stringOrFirstString(item: string | string[] | undefined) {
   return Array.isArray(item) ? item[0] : item;
 }
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method !== "GET") {
-    res.status(405).appendHeader("Allow", "GET").end();
-    return;
-  }
+export default makeApiHandler({
+  GET: async (req, res) => {
+    const queryParams = req.query;
+    const token_hash = stringOrFirstString(queryParams.token_hash);
+    const type = stringOrFirstString(queryParams.type);
 
-  const queryParams = req.query;
-  const token_hash = stringOrFirstString(queryParams.token_hash);
-  const type = stringOrFirstString(queryParams.type);
+    let next = "/error";
 
-  let next = "/error";
-
-  if (token_hash && type) {
-    const supabase = createClient(req, res);
-    const { error } = await supabase.auth.verifyOtp({
-      type: type as EmailOtpType,
-      token_hash,
-    });
-    if (error) {
-      console.error(error);
-    } else {
-      next = stringOrFirstString(queryParams.next) || "/";
+    if (token_hash && type) {
+      const supabase = createClient(req, res);
+      const { error } = await supabase.auth.verifyOtp({
+        type: type as EmailOtpType,
+        token_hash,
+      });
+      if (error) {
+        console.error(error);
+      } else {
+        next = stringOrFirstString(queryParams.next) || "/";
+      }
     }
-  }
 
-  res.redirect(next);
-}
+    res.redirect(next);
+  },
+});
