27f7bf00dafd9883fb2781767c453b2a4a996577
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-18 08:12:41 -0700

    Add makeProtectedApiHandler helper

--- src/lib/makeProtectedApiHandler.ts
+++ src/lib/makeProtectedApiHandler.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { sendError } from "./sendError";
+import { getAuthenticatedUser } from "./supabase/api";
 
 type Method = "GET" | "POST" | "PUT" | "DELETE";
 
@@ -8,16 +9,20 @@ type MethodHandler = (
   res: NextApiResponse
 ) => void | Promise<void>;
 
-export function makeApiHandler(
+export function makeProtectedApiHandler(
   methodHandlers: Partial<Record<Method, MethodHandler>>
 ) {
-  return (req: NextApiRequest, res: NextApiResponse) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
     const methodHandler = (
       methodHandlers as Partial<Record<string, MethodHandler>>
     )[req.method ?? ""];
     if (!methodHandler) {
       res.setHeader("Allow", Object.keys(methodHandlers).join(", "));
       return sendError(res, 405);
+    }
+    const user = await getAuthenticatedUser(req, res);
+    if (!user) {
+      return sendError(res, 401);
     }
     try {
       return methodHandler(req, res);
