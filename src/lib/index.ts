cdd9828433cdfc836717ccf77a2a61efd144fb07
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-17 17:29:05 -0700

    Add dateStringToDate helper

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,4 @@
+export { dateStringToDate } from "./dateStringToDate";
 export { makeApiHandler } from "./makeApiHandler";
 export { prisma } from "./prisma";
 export { sendError } from "./sendError";
