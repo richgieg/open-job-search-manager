56b022486bf0a25c03591e960765fb49bbd9a85b
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-19 08:36:14 -0700

    Validate /api/auth/confirm input

--- src/pages/api/auth/confirm.ts
+++ src/pages/api/auth/confirm.ts
@@ -1,27 +1,28 @@
 import { createClient } from "@/lib/supabase/api";
-import { makeApiHandler } from "@/lib";
+import { makeApiHandler, sendError } from "@/lib";
+import { z } from "zod";
 
-function stringOrFirstString(item: string | string[] | undefined) {
-  return Array.isArray(item) ? item[0] : item;
-}
+const querySchema = z.object({
+  token_hash: z.string(),
+});
 
 export default makeApiHandler({
   GET: async (req, res) => {
-    const queryParams = req.query;
-    const token_hash = stringOrFirstString(queryParams.token_hash);
-    let next = "/error";
-    if (token_hash) {
-      const supabase = createClient(req, res);
-      const { error } = await supabase.auth.verifyOtp({
-        type: "email",
-        token_hash,
-      });
-      if (error) {
-        console.error(error);
-      } else {
-        next = "/";
-      }
+    const parseResult = querySchema.safeParse(req.query);
+    if (!parseResult.success) {
+      return sendError(res, 400);
     }
-    res.redirect(next);
+    const { token_hash } = parseResult.data;
+    const supabase = createClient(req, res);
+    const { error } = await supabase.auth.verifyOtp({
+      type: "email",
+      token_hash,
+    });
+    if (error) {
+      console.error(error);
+      res.redirect("/error");
+      return;
+    }
+    res.redirect("/");
   },
 });
