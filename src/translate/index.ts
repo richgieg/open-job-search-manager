aca0eeb35d2d698a27fa3ff476566df1ed52998f
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-19 17:13:54 -0700

    Add CONTACT_MESSAGE_TYPES

--- src/translate/index.ts
+++ src/translate/index.ts
@@ -1,14 +1,22 @@
+import type {
+  JobStatus,
+  JobArrangement,
+  JobType,
+  ResumeTemplate,
+  ContactMessageType,
+} from "@/generated/prisma";
+
 export const t = {
   jobType: {
     fullTime: "Full-Time",
     partTime: "Part-Time",
     contract: "Contract",
-  },
+  } satisfies { [Key in JobType]: string },
   jobArrangement: {
     onSite: "On-Site",
     hybrid: "Hybrid",
     remote: "Remote",
-  },
+  } satisfies { [Key in JobArrangement]: string },
   jobStatus: {
     notApplied: "Not Applied",
     applied: "Applied",
@@ -19,11 +27,17 @@ export const t = {
     interviewedRejected: "Interviewed - Rejected",
     offerReceived: "Offer Received",
     offerAccepted: "Offer Accepted",
-  },
+  } satisfies { [Key in JobStatus]: string },
   resumeTemplates: {
     template01: "Template 01",
     template02: "Template 02",
-  },
+  } satisfies { [Key in ResumeTemplate]: string },
+  contactMessageTypes: {
+    support: "Support",
+    feedback: "Feedback",
+    featureRequest: "Feature Request",
+    other: "Other",
+  } satisfies { [Key in ContactMessageType]: string },
   profileNamePlaceholder: "[No Name]",
   resumeNamePlaceholder: "[No Name]",
   jobTitlePlaceholder: "[No Title]",
