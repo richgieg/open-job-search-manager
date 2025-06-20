3bb5e577b792c1fc753227f6e086f474772ee1e9
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-19 17:51:20 -0700

    Add functionality to ContactPage

---
+++ src/components/DropdownInput.tsx
@@ -0,0 +1,42 @@
+import { InputLabelText } from "./InputLabelText";
+
+type Props<T extends string | null> = {
+  label: string;
+  value: T;
+  setValue: (text: T) => void;
+  options: readonly Exclude<T, null>[];
+  translations: { [P in Exclude<T, null>]: string };
+  includeEmptyOption?: boolean;
+  disabled?: boolean;
+};
+
+export function DropdownInput<T extends string | null>({
+  label,
+  value,
+  setValue,
+  options,
+  translations,
+  includeEmptyOption = false,
+  disabled,
+}: Props<T>) {
+  return (
+    <label>
+      <InputLabelText text={label} />
+      <select
+        className="w-full px-2 py-1 border-1 focus:outline-none focus:ring-3 focus:ring-blue-300 focus:ring-offset-0"
+        value={value ?? ""}
+        onChange={(e) =>
+          setValue((e.target.value ? e.target.value : null) as T)
+        }
+        disabled={disabled}
+      >
+        {includeEmptyOption && <option value=""></option>}
+        {options.map((o) => (
+          <option key={o} value={o}>
+            {translations[o]}
+          </option>
+        ))}
+      </select>
+    </label>
+  );
+}
