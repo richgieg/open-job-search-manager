a47ff9c1a49b5e4f0c7ed49f1d6da2fa23ec257f
Richard Gieg <richgieg@users.noreply.github.com>
2025-06-21 09:32:56 -0700

    Create useLinkMutations

--- src/actual-pages/JobPage/MainContent.tsx
+++ src/actual-pages/JobPage/MainContent.tsx
@@ -2,7 +2,6 @@ import {
   ApplicationQuestion,
   Contact,
   Job,
-  Link,
   Profile,
   Resume,
 } from "@/generated/prisma";
@@ -15,6 +14,7 @@ import { SectionHeading } from "@/components/SectionHeading";
 import { ResumeOverview } from "./ResumeOverview";
 import { t } from "@/translate";
 import { useFullJobContext } from "./FullJobContext";
+import { useLinkMutations } from "./useLinkMutations";
 
 type Props = {
   profiles: Profile[];
@@ -23,6 +23,9 @@ type Props = {
 export function MainContent({ profiles }: Props) {
   const { fullJob, mutateFullJob } = useFullJobContext();
   const [profilePid, setProfilePid] = useState<string>(profiles[0]?.pid ?? "");
+
+  const { createLink, deleteLink, updateLink, moveLinkUp, moveLinkDown } =
+    useLinkMutations();
 
   const updateJob = async (job: Job) => {
     mutateFullJob({ ...fullJob, ...job }, { revalidate: false });
@@ -85,108 +88,6 @@ export function MainContent({ profiles }: Props) {
     );
     const response = await fetch(`/api/resumes/${resume.pid}`, {
       method: "DELETE",
-    });
-    if (!response.ok) {
-      mutateFullJob(fullJob, { revalidate: true });
-    }
-  };
-
-  const createLink = async () => {
-    const response = await fetch(`/api/jobs/${fullJob.pid}/links`, {
-      method: "POST",
-    });
-    const link: Link = await response.json();
-    mutateFullJob(
-      {
-        ...fullJob,
-        links: [...fullJob.links, link],
-      },
-      { revalidate: false }
-    );
-  };
-
-  const updateLink = async (link: Link) => {
-    mutateFullJob(
-      {
-        ...fullJob,
-        links: fullJob.links.map((l) => {
-          if (l.id === link.id) {
-            return link;
-          } else {
-            return l;
-          }
-        }),
-      },
-      { revalidate: false }
-    );
-    const response = await fetch(`/api/links/${link.pid}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(link),
-    });
-    if (!response.ok) {
-      mutateFullJob(fullJob, { revalidate: true });
-    }
-  };
-
-  const deleteLink = async (link: Link) => {
-    mutateFullJob(
-      {
-        ...fullJob,
-        links: fullJob.links.filter((l) => l.id !== link.id),
-      },
-      { revalidate: false }
-    );
-    const response = await fetch(`/api/links/${link.pid}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-      mutateFullJob(fullJob, { revalidate: true });
-    }
-  };
-
-  const moveLinkUp = async (link: Link) => {
-    const links = [...fullJob.links];
-    const index = links.findIndex((item) => item.id === link.id);
-    if (index > 0) {
-      const swapIndex = index - 1;
-      [links[index], links[swapIndex]] = [links[swapIndex], links[index]];
-    } else {
-      links.push(links.shift()!);
-    }
-    mutateFullJob({ ...fullJob, links }, { revalidate: false });
-    const orderedPids = links.map((item) => item.pid);
-    const response = await fetch(`/api/jobs/${fullJob.pid}/links/order`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ orderedPids }),
-    });
-    if (!response.ok) {
-      mutateFullJob(fullJob, { revalidate: true });
-    }
-  };
-
-  const moveLinkDown = async (link: Link) => {
-    const links = [...fullJob.links];
-    const index = links.findIndex((item) => item.id === link.id);
-    if (index < links.length - 1) {
-      const swapIndex = index + 1;
-      [links[index], links[swapIndex]] = [links[swapIndex], links[index]];
-    } else {
-      links.unshift(links.pop()!);
-    }
-    mutateFullJob({ ...fullJob, links }, { revalidate: false });
-    const orderedPids = links.map((item) => item.pid);
-    const response = await fetch(`/api/jobs/${fullJob.pid}/links/order`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ orderedPids }),
     });
     if (!response.ok) {
       mutateFullJob(fullJob, { revalidate: true });
